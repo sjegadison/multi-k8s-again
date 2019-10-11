dd7716d140432cb7a8bc2674e51c7d31636000bb
sjegadison <sivaguru.jegadison@gmail.com>
2019-10-11 18:12:44 +0530

    Ingress File Change

--- server/index.js
+++ server/index.js
@@ -35,23 +35,23 @@ const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/', (req, res) => {
+app.get('/be/', (req, res) => {
   res.send('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
+app.get('/be/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/be/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
-app.post('/values', async (req, res) => {
+app.post('/be/values', async (req, res) => {
   const index = req.body.index;
 
   if (parseInt(index) > 40) {
